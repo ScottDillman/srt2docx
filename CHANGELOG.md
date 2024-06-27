f5a13b31bd6d9b73c641ab92d859b2c82335d579
ScottDillman <scott@bitwise.ninja>
2024-06-26 23:40:57 -0400

    version update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
 
-## 0.1.0 (unreleased)
+## 0.1.0 2024-06-26
 - Initial release
+
+## 0.1.1 2024-06-26
+- Fix time format
+- add tests
--- srt2docx.py
+++ srt2docx.py
@@ -64,7 +64,7 @@ parser = argparse.ArgumentParser(
 )
 
 ## update me on major changes
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __contact__ = "scott@bitwise.ninja"
 __web__ = "https://dreamcyclesetudios.com"
 
